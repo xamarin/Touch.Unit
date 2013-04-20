932bb6092302e926dffbf8b03db2c388edd71868
Johannes Rudolph <jojo.rudolph@gmail.com>
2013-04-20 18:45:57 +0200

    Experimental test reporter
    Make it compile….
    
    Handle suite begin and end
    
    Move test status reporting out into a separate TestReporter that can be injected as well. Provide DefaulTestReporter that reports status and stats per suite.

---
+++ NUnitLite/TouchRunner/DefaultTestListener.cs
@@ -0,0 +1,97 @@
+using System;
+using NUnit.Framework.Api;
+using System.IO;
+using NUnit.Framework.Internal;
+using MonoTouch.NUnit.UI;
+
+namespace MonoTouch.NUnit
+{
+	public class DefaultTestListener : ITestReporter
+	{
+		int passed;
+		int failed;
+		int ignored;
+		int inconclusive;
+
+		public DefaultTestListener (TextWriter writer)
+		{
+			this.Writer = writer;
+		}
+		
+		public TextWriter Writer { get; private set; }
+
+		public void TestStarted (ITest test)
+		{
+			if (test is TestSuite) {
+				Writer.WriteLine ();
+				Writer.WriteLine (test.Name);
+			}
+		}
+		
+		public void TestFinished (ITestResult r)
+		{
+			TestResult result = r as TestResult;
+
+			if (result.Test is TestSuite) {
+				if (!result.IsFailure () && !result.IsSuccess () && !result.IsInconclusive () && !result.IsIgnored ())
+					Writer.WriteLine ("\t[INFO] {0}", result.Message);
+				
+				string name = result.Test.Name;
+				if (!String.IsNullOrEmpty (name))
+					Writer.WriteLine ("{0} : {1} ms", name, result.Time * 1000);
+			} else {
+				if (result.IsSuccess ()) {
+					Writer.Write ("\t[PASS] ");
+					passed++;
+				} else if (result.IsIgnored ()) {
+					Writer.Write ("\t[IGNORED] ");
+					ignored++;
+				} else if (result.IsFailure ()) {
+					Writer.Write ("\t[FAIL] ");
+					failed++;
+				} else if (result.IsInconclusive ()) {
+					Writer.Write ("\t[INCONCLUSIVE] ");
+					inconclusive++;
+				} else {
+					Writer.Write ("\t[INFO] ");
+				}
+				Writer.Write (result.Test.Name);
+				
+				string message = result.Message;
+				if (!String.IsNullOrEmpty (message)) {
+					Writer.Write (" : {0}", message.Replace ("\r\n", "\\r\\n"));
+				}
+				Writer.WriteLine ();
+				
+				string stacktrace = result.StackTrace;
+				if (!String.IsNullOrEmpty (result.StackTrace)) {
+					string[] lines = stacktrace.Split (new char [] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+					foreach (string line in lines)
+						Writer.WriteLine ("\t\t{0}", line);
+				}
+			}
+		}
+
+		public void TestOutput (TestOutput testOutput)
+		{
+		}
+
+		public void TestSuiteStarted (TestSuite ts)
+		{
+			passed = 0;
+			inconclusive = 0;
+			failed = 0;
+			ignored = 0;
+
+			Writer.WriteLine ("Started Suite: " + ts.Name);
+		}
+
+		public void TestSuiteFinished (TestSuite ts)
+		{
+			int total = passed + inconclusive + failed; // ignored are *not* run
+			Writer.WriteLine ("Finished Suite: " + ts.Name);
+			Writer.WriteLine ("Tests run: {0} Passed: {1} Inconclusive: {2} Failed: {3} Ignored: {4}", total, passed, inconclusive, failed, ignored);
+		}
+	}
+}
+
