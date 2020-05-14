45368d564fede9af3dd91c67ad085218886f7358
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-05-14 15:43:01 +0200

    [TouchRunner] Update the current element even if showing a sub element with failure info.
    Otherwise the current element will show up as not executed when returning from the sub element.

--- NUnitLite/TouchRunner/TestCaseElement.cs
+++ NUnitLite/TouchRunner/TestCaseElement.cs
@@ -60,7 +60,9 @@ namespace MonoTouch.NUnit.UI {
 					};
 					var dvc = new DialogViewController (root, true) { Autorotate = true };
 					runner.NavigationController.PushViewController (dvc, true);
-				} else if (GetContainerTableView () != null) {
+				}
+				// we still need to update our current element
+				if (GetContainerTableView () != null) {
 					var root = GetImmediateRootElement ();
 					root.Reload (this, UITableViewRowAnimation.Fade);
 				}
